34553455b0e8120cc6310eec165275f34f06e5f0
aeneas <pilgrimaeneas@gmail.com>
2025-10-20 15:17:55 +0300

    Finish 1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -8,40 +8,42 @@ const App = () => {
   const exercises3 = 14
 
   return (
-    <div>
+    <>
       <Header course={course} />
       <Content
         part1={part1} part2={part2} part3={part3}
-        exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}
+        exCount1={exercises1} exCount2={exercises2} exCount3={exercises3}
       />
-      <Total exCount={[exercises1, exercises2, exercises3]} />
-    </div>
+      <Total exCounts={[exercises1, exercises2, exercises3]} />
+    </>
   )
 }
 
 const Header = (props) => {
   return (
-    <h1>
-      {props.course}
-    </h1>
+    <h1>{props.course}</h1>
   )
 }
 
 const Content = (props) => {
   return (
     <>
-      <p>{props.part1} {props.exercises1}</p>
-      <p>{props.part2} {props.exercises2}</p>
-      <p>{props.part3} {props.exercises3}</p>
+      <Part part={props.part1} exCount={props.exCount1} />
+      <Part part={props.part2} exCount={props.exCount2} />
+      <Part part={props.part3} exCount={props.exCount3} />
     </>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>{props.part} {props.exCount}</p>
   )
 }
 
 const Total = (props) => {
   return (
-    <p>
-      Number of exercises {props.exCount.reduce((acc, x) => (acc + x), 0)}
-    </p>
+    <p>Number of exercises {props.exCounts.reduce((acc, x) => (acc + x), 0)}</p>
   )
 }
 
