2edbb948e819a4dfc4ccbec1f26e5ea093101969
aeneas <pilgrimaeneas@gmail.com>
2025-10-30 20:14:44 +0300

    Finish Part 1c

--- part1/part-materials/part1/src/App.jsx
+++ part1/part-materials/part1/src/App.jsx
@@ -1,24 +1,29 @@
-const Hello = (props) => {
-  console.log(props)
+import { useState } from "react"
+
+const App = () => {
+  const [counter, setCounter] = useState(0)
+  const incrementCounter = () => setCounter(counter + 1)
+  const decrementCounter = () => setCounter(counter - 1)
+  const resetCounter = () => setCounter(0)
+
   return (
+    // Here be the JSX Template. Don't define the functions here.
     <>
-      <p>
-        Hello {props.name}, you are {props.age} years old
-      </p>
+      <Display counter={counter} />
+      <Button name="Increment" onClick={incrementCounter} />
+      <Button name="Decrement" onClick={decrementCounter} />
+      <Button name="Reset" onClick={resetCounter} />
     </>
   )
 }
 
-const App = () => {
-  const name = 'Peter'
-  const age = 10
-  return (
-    <>
-      <h1>Greetings</h1>
-      <Hello name='Maya' age={26 + 10} />
-      <Hello name={name} age={age} />
-    </>
-  )
-}
+/* Calling a function that changes the state causes 
+    the component to re-render, recursively re-rendering its components.*/
+
+const Display = ({ counter }) => <p>{counter}</p>
+const Button = ({ name, onClick }) => <button onClick={onClick}>{name}</button>
+
+// Remember to keep your components small, modular and reusable across the app,
+// and even across multiple projects.
 
 export default App
