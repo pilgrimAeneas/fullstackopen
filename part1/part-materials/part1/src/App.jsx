9ffa16ea5274ed065b2113905c4ba11ae74b0b7c
aeneas <pilgrimaeneas@gmail.com>
2025-10-31 18:36:50 +0300

    Finished part1 c

--- part1/part-materials/part1/src/App.jsx
+++ part1/part-materials/part1/src/App.jsx
@@ -7,7 +7,6 @@ const App = () => {
   const resetCounter = () => setCounter(0)
 
   return (
-    // Here be the JSX Template. Don't define the functions here.
     <>
       <Display counter={counter} />
       <Button name="Increment" onClick={incrementCounter} />
@@ -17,13 +16,7 @@ const App = () => {
   )
 }
 
-/* Calling a function that changes the state causes 
-    the component to re-render, recursively re-rendering its components.*/
-
 const Display = ({ counter }) => <p>{counter}</p>
 const Button = ({ name, onClick }) => <button onClick={onClick}>{name}</button>
-
-// Remember to keep your components small, modular and reusable across the app,
-// and even across multiple projects.
 
 export default App
