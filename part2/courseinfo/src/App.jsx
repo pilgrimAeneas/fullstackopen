844cbfee0c19b3da11d00608ec34e4e0862e08ff
aeneas <pilgrimaeneas@gmail.com>
2025-11-07 19:38:34 +0300

    Finish 2.1

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,67 @@
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      },
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+const Course = ({ course }) => {
+  return (
+    <>
+      <Header course={course} />
+      <Content course={course} />
+      <Total course={course} />
+    </>
+  )
+}
+
+const Header = ({ course }) => {
+  return (
+    <h1>{course.name}</h1>
+  )
+}
+
+const Content = ({ course }) => {
+  return (
+    <>
+      {course.parts.map(part => <Part key={part.id} part={part} />)}
+    </>
+  )
+}
+
+const Part = ({ part: { name, exercises } }) => {
+  return (
+    <p>{name} {exercises}</p>
+  )
+}
+
+const Total = ({ course: { parts } }) => {
+  return (
+    <p>
+      Number of exercises {
+        parts.reduce((acc, part) => (acc + part.exercises), 0)
+      }
+    </p>
+  )
+}
+
+export default App
