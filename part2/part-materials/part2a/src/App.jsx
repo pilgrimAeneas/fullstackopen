386f4357adb275f2184b8a659f5f9bbb39db28d7
aeneas <pilgrimaeneas@gmail.com>
2025-11-07 19:15:51 +0300

    Finish part2a

--- part2/part-materials/part2a/src/App.jsx
+++ part2/part-materials/part2a/src/App.jsx
@@ -1,6 +1,13 @@
-const App = () => {
+import Note from "./components/Note"
+
+const App = ({ notes }) => {
   return (
-    <>hello</>
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map((note) => <Note key={note.id} content={note.content} />)}
+      </ul>
+    </div>
   )
 }
 
