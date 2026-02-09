98925781002769c59526512a25e654067315c0c8
aeneas <pilgrimaeneas@gmail.com>
2026-02-09 17:25:19 +0300

    Finish studying 3.a

---
+++ part3/hello-npm/server.js
@@ -0,0 +1,93 @@
+const express = require("express")
+const app = express()
+
+let notes = [
+  {
+    id: "1",
+    content: "HTML is easy",
+    important: true
+  },
+  {
+    id: "2",
+    content: "Browser can execute only JavaScript",
+    important: false
+  },
+  {
+    id: "3",
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true
+  }
+]
+
+app.use(express.json()) // mutates state in express() object to parse json??
+
+app.get("/", (request, response) => {
+  response.send("<h1>Hello, world!</h1>")
+})
+
+// CREATE
+app.post("/api/notes", (request, response) => {
+
+  if (!request.body.content) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+
+  const note = {
+    content: request.body.content,
+    important: request.body.important || false,
+    id: generateId()
+  }
+
+  notes = notes.concat(note)
+  response.json(note)
+})
+
+const generateId = () => {
+  const id = String(
+    (notes.length > 0
+      ? Math.max(...notes.map(note => Number(note.id)))
+      : 0)
+    + 1
+  )
+
+  return id
+}
+
+// READ
+app.get("/api/notes", (request, response) => {
+  response.json(notes)
+})
+
+app.get("/api/notes/:id", (request, response) => {
+  const id = request.params.id
+  const note = notes.find(note => note.id === id)
+
+  if (note) {
+    response.json(note)
+  } else {
+    response.statusMessage = "ID not found dude"
+    response.status(404).end()
+  }
+})
+
+
+// UPDATE
+
+
+// DELETE
+app.delete("/api/notes/:id", (request, response) => {
+  const id = request.params.id
+  notes = notes.filter(note => note.id !== id)
+
+  response.status(204).end()
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
+
+// This model is a straightforward CRUD API, that can be called
+// resource-oriented architecture
