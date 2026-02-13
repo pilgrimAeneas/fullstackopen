586a22d08f38e16c93d0b2b20e828e7872a7f8ca
aeneas <pilgrimaeneas@gmail.com>
2026-02-13 19:43:56 +0300

    Add build:ui script and else

--- part2/part-materials/part2/src/services/notes.js
+++ part2/part-materials/part2/src/services/notes.js
@@ -1,20 +1,20 @@
 import axios from "axios"
 
-const baseUrl = "http://localhost:3001/notes"
+const baseUrl = "/api/notes"
 
 const getAll = () =>
   axios
     .get(baseUrl)
-    .then(response => response.data) // returns a promise, whose callback will get response.data
+    .then(response => response.data)
 
 const create = newObject =>
   axios
     .post(baseUrl, newObject)
-    .then(response => response.data) // returns a promise, whose callback will get response.data
+    .then(response => response.data)
 
 const update = (id, newObject) =>
   axios
     .put(`${baseUrl}/${id}`, newObject)
-    .then(response => response.data) // returns a promise, whose callback will get response.data
+    .then(response => response.data)
 
 export default { getAll, create, update }
